b59f5f5a0469f36880b70c928e21beea4e23cbfc
apple <apple@aca8002b.ipt.aol.com>
2017-06-16 09:57:04 +0800

    Initial Commit

---
+++ runTimeTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  runTimeTest
+//
+//  Created by apple on 2017/6/16.
+//  Copyright © 2017年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ runTimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runTimeTest
+//
+//  Created by apple on 2017/6/16.
+//  Copyright © 2017年 YY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
